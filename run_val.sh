4fd4bfcbee316546bbdd6545434aa8782fd09973
yuchen-mei <yuchen.mei@outlook.com>
2024-03-18 22:31:33 -0700

    bn folder and run val script

---
+++ run_val.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+for dir in "./imagenet/val" "./vww-s256/val"; do
+    if [ ! -d "$dir" ]; then
+        echo "Error: $dir does not exist."
+        exit 1
+    fi
+done
+
+
+echo -e "\nTesting ImageNet0"
+python3 eval_torch.py --net_id mcunet-in0 --dataset imagenet --data-dir ./imagenet/val
+python3 eval_torch.py --net_id mcunet-in0 --dataset imagenet --data-dir ./imagenet/val --bfloat16
+
+echo -e "\nTesting ImageNet1"
+python3 eval_torch.py --net_id mcunet-in1 --dataset imagenet --data-dir ./imagenet/val
+python3 eval_torch.py --net_id mcunet-in1 --dataset imagenet --data-dir ./imagenet/val --bfloat16
+
+echo -e "\nTesting ImageNet2"
+python3 eval_torch.py --net_id mcunet-in2 --dataset imagenet --data-dir ./imagenet/val
+python3 eval_torch.py --net_id mcunet-in2 --dataset imagenet --data-dir ./imagenet/val --bfloat16
+
+echo -e "\nTesting ImageNet3"
+python3 eval_torch.py --net_id mcunet-in3 --dataset imagenet --data-dir ./imagenet/val
+python3 eval_torch.py --net_id mcunet-in3 --dataset imagenet --data-dir ./imagenet/val --bfloat16
+
+echo -e "\nTesting ImageNet4"
+python3 eval_torch.py --net_id mcunet-in4 --dataset imagenet --data-dir ./imagenet/val
+python3 eval_torch.py --net_id mcunet-in4 --dataset imagenet --data-dir ./imagenet/val --bfloat16
+
+echo -e "\nTesting VWW0"
+python3 eval_torch.py --net_id mcunet-vww0 --dataset vww --data-dir ./vww-s256/val
+python3 eval_torch.py --net_id mcunet-vww0 --dataset vww --data-dir ./vww-s256/val --bfloat16
+
+echo -e "\nTesting VWW1"
+python3 eval_torch.py --net_id mcunet-vww1 --dataset vww --data-dir ./vww-s256/val
+python3 eval_torch.py --net_id mcunet-vww1 --dataset vww --data-dir ./vww-s256/val --bfloat16
+
+echo -e "\nTesting VWW2"
+python3 eval_torch.py --net_id mcunet-vww2 --dataset vww --data-dir ./vww-s256/val
+python3 eval_torch.py --net_id mcunet-vww2 --dataset vww --data-dir ./vww-s256/val --bfloat16
+
+# int8
+# python3 eval_tflite.py --net_id mcunet-in4 --dataset imagenet --data-dir ./imagenet/val
